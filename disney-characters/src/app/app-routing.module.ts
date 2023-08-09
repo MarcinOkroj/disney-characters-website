37e07d5c0a5a37080445c677e65862458dd5e184
MarcinOkroj <114299230+MarcinOkroj@users.noreply.github.com>
2023-08-09 18:38:49 +0200

    feat: layout (HTML) and folders structure
    Add folders structure (components, shared, assets),
    Add header and footer,
    Add basic routing,
    Add Material.

--- disney-characters/src/app/app-routing.module.ts
+++ disney-characters/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FavouritesComponent } from './favourites/favourites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'favourites', component: FavouritesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- disney-characters/src/app/app.module.ts
+++ disney-characters/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule }from './shared/material/material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FavouritesComponent } from './favourites/favourites.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    FavouritesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
